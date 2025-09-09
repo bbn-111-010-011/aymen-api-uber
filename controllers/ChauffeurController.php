a0526b5e40b8ca57df883aca9dc2c6951a7aa252
Mister BRUNO <brunopaul1412@gmail.com>
2025-09-09 14:53:48 +0200

    Création du endpoint PUT /chauffeurs/{id}

--- controllers/ChauffeurController.php
+++ controllers/ChauffeurController.php
@@ -27,6 +27,16 @@ class ChauffeurController
         http_response_code(201);
         echo json_encode($ligneChauffeur);
     }
+
+        public function updateChauffeur($id, $data) {
+            $success=$this->model->updateDBChauffeur($id, $data);
+            if ($success) {
+                http_response_code(204);
+            } else { 
+                http_response_code(404);
+                echo json_encode(["message" => "Chauffeur non trouvé ou non modifié"]);
+            }
+    }
 }
 // $chauffeurController = new ChauffeurController();
 // $chauffeurController->getAllChauffeurs();
